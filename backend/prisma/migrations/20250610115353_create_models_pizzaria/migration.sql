7cc621b9921db61f947d82b7a53a1f3b21af2d43
Beatriz Costa <anabeatrizqcosta@hotmail.com>
2025-06-16 09:45:33 -0300

    :sparkles: Primero commit

---
+++ backend/prisma/migrations/20250610115353_create_models_pizzaria/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `categories` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `items` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `orders` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `products` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "items" DROP CONSTRAINT "items_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "items" DROP CONSTRAINT "items_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "products" DROP CONSTRAINT "products_category_id_fkey";
+
+-- DropTable
+DROP TABLE "categories";
+
+-- DropTable
+DROP TABLE "items";
+
+-- DropTable
+DROP TABLE "orders";
+
+-- DropTable
+DROP TABLE "products";
+
+-- DropTable
+DROP TABLE "users";
