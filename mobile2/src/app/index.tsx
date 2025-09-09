e1e9b2cc41a96991bfe8ea5cd7e887d9cf229bcb
Isaac Menezes <isaac.m.silva10@aluno.senai.br>
2025-09-09 08:24:49 -0300

    :pencil: Reedição - Dashboard

---
+++ mobile2/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>
+                Hello, this is the mobile app!
+            </Text>
+        </View>
+    );
+}
