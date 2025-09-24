ea3465592dfd5b575fc884fb0c8c616eea8027c8
isaacmenezessenai <isaacmorngstar@gmail.com>
2025-09-23 21:53:55 -0300

    :bricks: Atualizando GIT IGNORE - AXIOS API

--- pizzaApp/src/services/api.ts
+++ pizzaApp/src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
+
 const api = axios.create({
-<<<<<<< HEAD
     baseURL: "http://192.168.137.1:3333",
-=======
-    baseURL: "http://172.25.62.21:3333",
->>>>>>> e2d586e231b7e5129e8e6697da2bcab136973324
-});
+})
 
 export {api}
