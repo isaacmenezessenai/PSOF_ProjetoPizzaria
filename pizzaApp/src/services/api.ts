7874d6fd5a3f379edf6c473377985ded18bf28d8
Ana Beatriz Queiroz Costa <ana.b.costa18@aluno.senai.br>
2025-10-07 11:32:38 -0300

    :bug: Correção banco de Dados para o pagamento

--- pizzaApp/src/services/api.ts
+++ pizzaApp/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http:// 10.216.170.76:3333",
-})
+    baseURL: "http://10.106.131.243:3333",
+});
 
-export {api}
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+
+console.log("TESTE DE URL LIDA:", API_BASE_URL);
+
+export { api }
