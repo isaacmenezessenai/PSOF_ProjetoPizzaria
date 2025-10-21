c8b34788f4464001bb79504dba6f0726d3808b6a
BeatrizCosta02 <ana.b.costa18@aluno.senai.br>
2025-10-21 08:20:47 -0300

    Variação de ip

--- pizzaApp/src/screens/StatusPedido.tsx
+++ pizzaApp/src/screens/StatusPedido.tsx
@@ -29,7 +29,7 @@ interface Order {
   [key: string]: any;
 }
 
-const API_HOSTS = ["http://10.106.131.254:3333"];
+const API_HOSTS = ["http://192.168.105.237:3333"];
 const POLLING_INTERVAL_MS = 3000;
 
 // Paleta de Cores (Pizzaria Artemis)
