84decce16eedd0932706ee30d511812b3588aa72
Arthur Pascoal Ribeiro <arthur.p.ribeiro@aluno.senai.br>
2025-10-28 13:43:23 -0300

    :recycle: refactor: retirando preço de ingredient

--- backend/src/services/ingredients/CreateIngredientService.ts
+++ backend/src/services/ingredients/CreateIngredientService.ts
@@ -5,12 +5,11 @@ import prismaClient from "../../prisma";
 
 interface IngredientRequest {
     name: string;
-    price: number; 
     banner: string;
 }
 
 class CreateIngredientService {
-    async execute({ name, price, banner}: IngredientRequest) {
+    async execute({ name, banner}: IngredientRequest) {
         if (!name || !banner) {
             throw new Error('Name and banner are required.');
         }
@@ -18,13 +17,11 @@ class CreateIngredientService {
         const ingredient = await prismaClient.ingredients.create({
             data: {
                 name: name,
-                price: price, 
                 banner: banner,
             },
             select: {
                 id: true,
                 name: true,
-                price: true, 
                 banner: true,
             }
         });
