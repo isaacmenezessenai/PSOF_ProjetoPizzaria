de93a86711cfc6be87507f3b5e84553ac7d33888
BeatrizCosta02 <ana.b.costa18@aluno.senai.br>
2025-11-21 23:28:42 -0300

    Status item ordem - Backend

--- pizzaApp/src/screens/StatusPedido.tsx
+++ pizzaApp/src/screens/StatusPedido.tsx
@@ -30,7 +30,7 @@ interface Order {
 }
 
 
-const API_HOSTS = ["http://192.168.3.118:3333"];
+const API_HOSTS = ["http://192.168.15.16:3333"];
 const POLLING_INTERVAL_MS = 3000;
 
 // Paleta de Cores (Pizzaria Artemis)
