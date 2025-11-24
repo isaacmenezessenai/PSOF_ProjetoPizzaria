2250fc822e34e0c78556b42730a96d0a4265fcb9
enzogoldnersenai <enzo.g.castro@aluno.senai.br>
2025-11-23 23:04:03 -0300

    :bugfix: arrumando erro de aparecer favorito em todas as contas e botão ver

--- backend/src/services/favorites/ListFavoriteService.ts
+++ backend/src/services/favorites/ListFavoriteService.ts
@@ -3,9 +3,13 @@ import prismaClient from "../../prisma";
 class ListFavoritesService {
   async execute(users_id: string) {
 
+    if (!users_id) {
+        throw new Error("ID do usuário é obrigatório para listar favoritos.");
+    }
+
     const favorites = await prismaClient.favorites.findMany({
       where: {
-        users_id: users_id
+        users_id: users_id 
       },
       orderBy:{
         created_at: 'desc'
